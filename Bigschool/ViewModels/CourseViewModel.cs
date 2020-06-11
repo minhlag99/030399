40056498bd1774f592021c7de7a43c81b71379b5
Administrator <Administrator@PC241>
2020-06-11 16:01:17 +0700

    gy

--- Bigschool/ViewModels/CourseViewModel.cs
+++ Bigschool/ViewModels/CourseViewModel.cs
@@ -12,8 +12,10 @@ namespace Bigschool.ViewModels
         [Required]
         public string Place { get; set; }
         [Required]
+        [FutureDate]
         public string Date { get; set; }
         [Required]
+        [ValidTime]
         public string Time { get; set; }
         [Required]
         public byte Category { get; set; }
@@ -23,4 +25,5 @@ namespace Bigschool.ViewModels
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
     }
+
 }
